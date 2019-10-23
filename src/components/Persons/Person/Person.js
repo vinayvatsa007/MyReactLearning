f042be0cdcd8c26473422f20e1d4748d350e89b2
vinayvatsa007 <vinay.vatsa.007@gmail.com>
2019-10-23 20:43:40 +0530

    converted person and persons from functional to class component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,15 +3,18 @@
  */
 import React,{Component} from 'react';
 import './Person.css';
-const person = (props)=>{
-    return(
-    <div className="Person">
-        <p onClick={props.click}>i am a {props.name} and age:{props.age}</p>
-        <p>{props.children}</p>
-        <input type="text" onChange={props.changed} value={props.name} />
-    </div>
-    );
-}
-export default person;
+
+class Person extends Component{
+    render(){
+        return(
+            <div className="Person">
+                <p onClick={this.props.click}>i am a {this.props.name} and age:{this.props.age}</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name} />
+            </div>
+        );
+    }
+};
+export default Person;
 
 
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Person from './Person/Person';
-// const Persons(props){
-//     return ();
-// } // same can be achieved using arrow function
-// const Persons(props)=>( ); // this will omit the return statement
 
-// click = {()=> props.deletePersonHandler(index)}
-// key = {person.id}
-// changed = {(event)=>props.nameChangedHandler(event,person.id)}
-const persons =(props)=> props.persons.map((person,index) => {
-    return <Person name={person.name}
-                   age={person.age}
-                   key={person.id}
-                   gender={person.gender}
-                   click={() => props.clicked(index)}
-                   changed={(event) => props.changed(event, person.id)}
-    />
-});
-export default persons;
+class Persons extends Component {
+    render(){
+        return this.props.persons.map((person,index) => {
+            return <Person name={person.name}
+                           age={person.age}
+                           key={person.id}
+                           gender={person.gender}
+                           click={() => this.props.clicked(index)}
+                           changed={(event) => this.props.changed(event, person.id)}
+            />
+        });
+    }
+}
+// const persons =(props)=> props.persons.map((person,index) => {
+//     return <Person name={person.name}
+//                    age={person.age}
+//                    key={person.id}
+//                    gender={person.gender}
+//                    click={() => props.clicked(index)}
+//                    changed={(event) => props.changed(event, person.id)}
+//     />
+// });
+export default Persons;
